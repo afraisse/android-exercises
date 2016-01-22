03099f45654575da47bd252eb2033daea53b218d
Adrian Fraisse <adrian.fraisse@etudiant.mines-nantes.fr>
2016-01-22 15:52:40 +0100

    Integration test with Espresso

--- app/src/androidTest/java/fr/android/androidexercises/LoginActivityTest.java
+++ app/src/androidTest/java/fr/android/androidexercises/LoginActivityTest.java
@@ -1,5 +1,16 @@
 package fr.android.androidexercises;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.core.deps.guava.collect.Iterables;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.runner.lifecycle.ActivityLifecycleMonitor;
+import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
+import android.support.test.runner.lifecycle.Stage;
 import android.test.ActivityInstrumentationTestCase2;
 
 public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginActivity> {
@@ -11,28 +22,40 @@ public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginAct
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        //injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
         getActivity();
     }
 
+    public void testLogin() throws Exception {
+        // Given
+        Espresso.onView(ViewMatchers.withId(R.id.usernameEdit)).perform(ViewActions.typeText("bouffon@killian.fr"));
+        Espresso.onView(ViewMatchers.withId(R.id.passwordEdit)).perform(ViewActions.typeText("password"));
+        // When
+        ViewActions.closeSoftKeyboard();
+        Espresso.onView(ViewMatchers.withId(R.id.loginButton)).perform(ViewActions.click());
+        // Then
+        Espresso.onView(ViewMatchers.withId(R.id.loggedText)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
 
-    public void takeScreenshot(String name) {
-        //Spoon.screenshot(getCurrentActivity(), name);
     }
 
-//    public Activity getCurrentActivity() {
-//        Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
-//        instrumentation.waitForIdleSync();
-//        final Activity[] activity = new Activity[1];
-//        instrumentation.runOnMainSync(new Runnable() {
-//            @Override
-//            public void run() {
-//                ActivityLifecycleMonitor activityLifecycleMonitor = ActivityLifecycleMonitorRegistry.getInstance();
-//                java.util.Collection<Activity> resumedActivities = activityLifecycleMonitor.getActivitiesInStage(Stage.RESUMED);
-//                activity[0] = Iterables.getOnlyElement(resumedActivities);
-//            }
-//        });
-//        return activity[0];
-//    }
+    /*public void takeScreenshot(String name) {
+        // Pour créer des rapports de test
+        Spoon.screenshot(getCurrentActivity(), name);
+    }*/
+
+   public Activity getCurrentActivity() {
+        Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
+        instrumentation.waitForIdleSync();
+        final Activity[] activity = new Activity[1];
+        instrumentation.runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+              ActivityLifecycleMonitor activityLifecycleMonitor = ActivityLifecycleMonitorRegistry.getInstance();
+                java.util.Collection<Activity> resumedActivities = activityLifecycleMonitor.getActivitiesInStage(Stage.RESUMED);
+                activity[0] = Iterables.getOnlyElement(resumedActivities);
+            }
+        });
+        return activity[0];
+    }
 
 }
--- app/src/test/java/fr/android/androidexercises/LoginActivityTest.java
+++ app/src/test/java/fr/android/androidexercises/LoginActivityTest.java
@@ -1,26 +1,52 @@
 package fr.android.androidexercises;
 
-import org.junit.Test;
+import junit.framework.Assert;
 
-import static org.junit.Assert.*;
+import org.assertj.android.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.shadows.ShadowToast;
 
 /**
- * Created by Adrian on 22/01/2016.
+ * Unit test the Login Activity
  */
+@RunWith(CustomRobolectricTestRunner.class)
 public class LoginActivityTest {
+
+    LoginActivity activity;
+
+    @Before
+    public void setUp() {
+        activity = Robolectric.setupActivity(LoginActivity.class);
+    }
 
     @Test
     public void testNotLogged() throws Exception {
-
+        // Given
+        // When
+        activity.notLogged();
+        // Then
+        Assertions.assertThat(activity.loginLayout).isVisible();
+        Assertions.assertThat(activity.loggedText).isGone();
     }
 
     @Test
     public void testLogged() throws Exception {
-
+        // When
+        activity.logged();
+        // Then
+        Assertions.assertThat(activity.loginLayout).isGone();
+        Assertions.assertThat(activity.loggedText).isVisible();
     }
 
     @Test
     public void testMessage() throws Exception {
-
+        // When
+        activity.message(R.string.text_logged);
+        // Then
+        String textOfLatestToast = ShadowToast.getTextOfLatestToast();
+        Assert.assertEquals(textOfLatestToast, "You are logged in.");
     }
 }
