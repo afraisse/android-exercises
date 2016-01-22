6c57206eaebf86c11aae30d90ea932825d33e73b
Adrian Fraisse <adrian.fraisse@etudiant.mines-nantes.fr>
2016-01-22 15:52:04 +0100

    Integration tests with Espresso

---
+++ app/src/test/java/fr/android/androidexercises/LoginActivityTest.java
@@ -0,0 +1,26 @@
+package fr.android.androidexercises;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Adrian on 22/01/2016.
+ */
+public class LoginActivityTest {
+
+    @Test
+    public void testNotLogged() throws Exception {
+
+    }
+
+    @Test
+    public void testLogged() throws Exception {
+
+    }
+
+    @Test
+    public void testMessage() throws Exception {
+
+    }
+}
